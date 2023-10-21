c408dd6754d1a3d289a1c6a25f8dbc45ee502a36
aaryan1203 <aaryanjain1203@gmail.com>
2023-10-20 22:52:39 -0400

    Deleted wrong folder

--- BeatBalance/App.js
+++ BeatBalance/App.js
@@ -1,13 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { ConnectToTerraButton } from "@tryterra/terra-ui";
 
 export default function App() {
   const terra = new Terra(process.env.DEV_ID, process.env.API_KEY);
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ConnectToTerraButton
+        onClick={async () => {
+          const url = (
+            await axios.get("http://localhost:3000/api/generateWidgetSession")
+          ).data.url;
+          window.location = url;
+        }}
+      />
     </View>
   );
 }
@@ -15,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
